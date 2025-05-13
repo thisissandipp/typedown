120a06e01083ed70d16823282d5e8c0b95bae155
Sandip <sandipramanik07@gmail.com>
2025-05-13 20:37:28 +0530

    feat: fetch and show documents list in sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,36 +3,26 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarGroupAction,
+  SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
   SidebarSeparator,
 } from '@/components/ui/sidebar';
-import { Plus, type LucideIcon } from 'lucide-react';
 import { NavUser } from '@/components/nav-user';
+import { SidebarDocument, User } from '@/types';
+import { FileText, Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { User } from '@/types';
 
 interface AppSidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   user: User | null;
+  documents: SidebarDocument[] | null;
 }
 
-interface SidebarGroupItem {
-  title: string;
-  actionIcon: LucideIcon;
-  actionTooltip: string;
-  destination: string;
-}
-
-const sidebarGroupItems: SidebarGroupItem[] = [
-  {
-    title: 'Personal Workspace',
-    actionIcon: Plus,
-    actionTooltip: 'Add a document',
-    destination: '/documents/new',
-  },
-];
-
-export function AppSidebar({ user }: AppSidebarProps) {
+export function AppSidebar({ user, documents }: AppSidebarProps) {
   const router = useRouter();
 
   return (
@@ -42,15 +32,30 @@ export function AppSidebar({ user }: AppSidebarProps) {
       </SidebarHeader>
       <SidebarSeparator />
       <SidebarContent>
-        {sidebarGroupItems.map((groupItem) => (
-          <SidebarGroup key={groupItem.title}>
-            <SidebarGroupLabel>{groupItem.title}</SidebarGroupLabel>
-            <SidebarGroupAction onClick={() => router.push(groupItem.destination)}>
-              <groupItem.actionIcon className="h-4 w-4" />
-              <span className="sr-only">{groupItem.actionTooltip}</span>
-            </SidebarGroupAction>
-          </SidebarGroup>
-        ))}
+        <SidebarGroup key="personal-workspace">
+          <SidebarGroupLabel>Personal Workspace</SidebarGroupLabel>
+          <SidebarGroupAction onClick={() => router.push('/documents/new')}>
+            <Plus className="h-4 w-4" />
+            <span className="sr-only">Add a document</span>
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            {!documents &&
+              Array.from({ length: 3 }, (_, index) => <SidebarMenuSkeleton key={index} />)}
+            <SidebarMenu>
+              {documents &&
+                documents.map((item) => (
+                  <SidebarMenuItem key={item.id}>
+                    <SidebarMenuButton size="sm" asChild>
+                      <a href={`/documents/${item.id}`}>
+                        <FileText className="text-sidebar-foreground/60" />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
